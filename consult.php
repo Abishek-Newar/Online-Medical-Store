fda9bd333d496a1397436109c49a1d286f209527
Abishek Newar <97790157+abishek1872@users.noreply.github.com>
2023-05-20 15:49:47 +0530

    Add files via upload

---
+++ consult.php
@@ -0,0 +1,39 @@
+<?php
+$server_name="localhost";
+$username="root";
+$password="";
+$database_name="medical website";
+
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+
+if(!$conn)
+{
+	die("Connection Failed:" . mysql_connect_errors());
+}
+
+if(isset($_POST['save']))
+{
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+      $email = $_POST['email'];
+	$address = $_POST['address'];
+	$doctorname = $_POST['doctorname'];
+	
+	$sql_query="INSERT INTO consult(name,phone,email,address,doctorname)
+	VALUES('$name','$phone','$email','$address','$doctorname')";
+	
+	if (mysqli_query($conn, $sql_query))
+	{
+		echo "";
+	}
+	else
+	{
+		echo "Error: " . $sql . "" . mysqli_error($conn);
+	}
+	mysqli_close($conn);
+}
+
+header("Location: booksucce.html");
+exit();
+?>
+
